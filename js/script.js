5693291cdb1b7fb73cda9d18cf47c6be546c4f33
ksfallon <fallon.katelin@gmail.com>
2021-05-16 21:08:11 -0400

    able to get text to save as a key value pair rather than an array that could be added to. And have the local storage appended in the input text space so it does not go away when you refresh the page

--- js/script.js
+++ js/script.js
@@ -1,16 +1,38 @@
-buttonNine = document.querySelector('#btn-nine')
-nineAmTask = document.querySelector("#nine-am");
+buttonNine = document.querySelector('#btn9')
+nineAmTask = document.querySelector("#text9");
+buttonTen = document.querySelector('#btn10')
 
 
 function localStorageNineAm(event) {
     event.preventDefault();
 
-    var eventNine = nineAmTask.value;
+    var taskLocal = nineAmTask.value
 
-    var localnineAm = JSON.parse(localStorage.getItem("nineAM")) || [];
-    localnineAm.push(eventNine)
-    localStorage.setItem("nineAM", JSON.stringify(localnineAm));
-    console.log("nineAM", localnineAm);
+    localStorage.setItem("text9", taskLocal);
+    console.log("text9", taskLocal);
+
 }
 
+function displayLocalStorage () {
+    var displayTask = localStorage.getItem("text9");
+    $("#text9").append(displayTask);
+}
+
+displayLocalStorage ();
+
+
 buttonNine.addEventListener('click', localStorageNineAm);
+
+// function localStorageNineAm(event) {
+//     event.preventDefault();
+
+//     var taskInput = $(this.parentNode).children('.time-event').attr('id');
+//     var taskLocal = $(this.parentNode).children('.time-event').val();
+
+//     localStorage.setItem(taskInput, taskLocal);
+//     console.log(taskInput, taskLocal);
+// }
+
+// buttonNine.addEventListener('click', localStorageNineAm);
+
+// buttonTen.addEventListener('click', localStorageNineAm);
