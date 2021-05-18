9351d26dea4bb588bf2cb7141102886c608c8d5a
ksfallon <fallon.katelin@gmail.com>
2021-05-18 16:05:25 -0400

    updated the rows and columns so they are no longer overlapping - fixed the icon problem, but still need to work on centering it, and really just simplified the HTML. Also did pseudo code for moment js to have the text area change depending on the time

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@ buttonNine = document.querySelector('#btn9')
 nineAmTask = document.querySelector("#text9");
 buttonTen = document.querySelector('#btn10')
 
+// PSEUDO CODE FOR TIME/COLOR CHANGE
+// using Moment.js
+// if time listed is the current hour: make background color red
+// if time listed already happened, in the past of current time: make background color grey
+// if time listed is in the future, hasn't happened in comparison to current time: make background color green
 
 function localStorageNineAm(event) {
     event.preventDefault();
@@ -23,3 +28,16 @@ displayLocalStorage ();
 
 buttonNine.addEventListener('click', localStorageNineAm);
 
+// function localStorageNineAm(event) {
+//     event.preventDefault();
+
+//     var taskInput = $(this.parentNode).children('.time-event').attr('id');
+//     var taskLocal = $(this.parentNode).children('.time-event').val();
+
+//     localStorage.setItem(taskInput, taskLocal);
+//     console.log(taskInput, taskLocal);
+// }
+
+// buttonNine.addEventListener('click', localStorageNineAm);
+
+// buttonTen.addEventListener('click', localStorageNineAm);
